d27f65dee161461777aac917fea250d64ba6f609
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2021-02-16 21:33:44 +0300

    ffasd

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 from datetime import date
 
-from models import *
-from forms import *
+# from models import *
+# from forms import *
 
 import config as config
+
+from flask_wtf import FlaskForm
+from wtforms import fields, validators
+from wtforms_alchemy import ModelForm
 
 __author__ = 'OlegKupavtsev'
 # Теория 41, 51 стр. конспекта.
@@ -16,6 +20,57 @@ app = Flask(__name__, template_folder='template')
 app.config.from_object(config)
 
 db = SQLAlchemy(app)
+
+
+# Models
+class Article(db.Model):    # Данный Класс наследуется от метода Model БД. Модель в Python это Таблица в БД
+    
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String(140), unique = True, nullable = False)
+    article_url = db.Column(db.String(300), unique = True, nullable = False)
+    content = db.Column(db.String(2000), nullable = False)
+    date_created = db.Column(db.Date, default = date.today)
+    
+    def __str__(self):  # Сам объект этого класса становится строкой ?
+        return '<Article: {}>'.format(self.title)
+
+class Comment(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+
+    article_id = db.Column(                      # 56 стр. конспекта
+                 db.Integer,                     # Integer потому что id(9стр) - Integer
+                 db.ForeignKey('article.id'),    # Внешний ключ к таблице Article .Связь с колонкой в 9ой строке.
+                 nullable = False,
+                 index = True)
+    article = db.relationship(Article, foreign_keys=[article_id, ]) # Отношение с Классом Article. Сама переменная
+                                                                    # является объктом класса Article
+    #27 При помощи внешенего ключа foreign_keys=[article_id, ] он понимает с каким классом Article устанавливается отношение
+    #27 Отношение будет является объектом класса Article, таким, что его id лежит в 22
+    #22 Мы возьмем article_id сделаем JOIN(соединение двух таблиц). И при помощи JOIN у нас будут данные из Article&Comment
+    
+    content = db.Column(db.String(3000), nullable = False)
+    date_created = db.Column(db.Date, default = date.today)
+
+    def __str__(self):
+        return '<Comment {} to Article{}>'.format(self.title, self.article.title)
+
+
+# FORMS
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment     
+        include = [         
+            'article_id',   
+            ]               
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
